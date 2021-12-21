0a5129d2146a4e4409e4fad27a4166527b8795f7
Yulai202020 <ngmyulai@gmail.com>
2021-12-21 13:51:31 +0500

    main

--- SEGOS/sys32/settings/settdata.py
+++
@@ -1,8 +0,0 @@
-import conf,json
-def read(pyth):
-    with open (pyth , "r") as files:
-        textjson = files.read() 
-        return textjson
-data = read(conf.pyths)
-data = json.loads(data)
-Logs = data["logs"];progfiles = data["pyth"]
--- SEGOS/sys32/utilits/osproperties.py
+++
@@ -1,6 +0,0 @@
-from data.datads import *
-def init():
-    print("Name OS :",nameos)
-    print("Version OS :",__version__)
-    print("Local user:",user)
-    print("User list",userlist)
